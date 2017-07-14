e072aa2c80ed8ef590b6cb9804dd71882433d624
jolson615 <jolson615@gmail.com>
2017-07-14 14:25:45 -0400

    Project w/ Pedro

--- FortuneTeller5/ViewController.swift
+++ FortuneTeller5/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Declare variables here
+    @IBOutlet weak var theFortune: UILabel!
+    @IBOutlet weak var theButton: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func buttonPress(_ sender: Any) {
+        theFortune.text = newFortune()
+        theFortune.textColor = UIColor.init(red: 255.0/255.0, green: 124.0/255.0, blue: 0.0/255.0, alpha: 1.0)
+    }
 
 }
+
+
+
+
+
+
+
+
+
+
+
 
